0f4bdd37247ab1181ebd4d050fd8872e41cb251e
kekekoby <jacobaluna8@gmail.com>
2025-11-19 15:12:33 +0800

    food stock integrated

--- model/FoodStockModel.java
+++ model/FoodStockModel.java
@@ -4,10 +4,18 @@ import util.DBConnector;
 import java.sql.*;
 import java.util.*;
 
+/**
+ * All the SQL statements.
+ */
 public class FoodStockModel {
-
-    public void addFood(String name, java.sql.Date expiry, java.sql.Date bought, double cost, double calories,  double qty) {
-        String sql = "INSERT INTO food_stock (food_name, date_expiry, date_bought, cost, caloric_count, stock_qty) VALUES (?, ?, ?, ?, ?, ?)";
+    // Add a food_stock record (one food item)
+    public void addFood(String name, java.sql.Date expiry, java.sql.Date bought,
+                        double cost, double calories, double qty) {
+        String sql = """
+            INSERT INTO food_stock 
+            (food_name, date_expiry, date_bought, cost, caloric_count, stock_qty)
+            VALUES (?, ?, ?, ?, ?, ?)
+        """;
         try (Connection conn = DBConnector.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
@@ -18,11 +26,14 @@ public class FoodStockModel {
             pstmt.setDouble(5, calories);
             pstmt.setDouble(6, qty);
             pstmt.executeUpdate();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-
+    /**
+     * Show all the food_stock records.
+     */
     public List<String> listFoods() {
         List<String> foods = new ArrayList<>();
         String qry = "SELECT * FROM food_stock";
@@ -33,19 +44,23 @@ public class FoodStockModel {
 
             while (rs.next()) {
                 foods.add(String.format(
-                    "ID: %d | %s | Exp: %s | calPerServing: %d | Qty: %.2f",
+                    "ID: %d | %s | Exp: %s | kCal: %d | Stock: %.2f | Cost: %.2f",
                     rs.getInt("food_id"),
                     rs.getString("food_name"),
                     rs.getDate("date_expiry"),
                     rs.getInt("caloric_count"),
-                    rs.getDouble("stock_qty")));
+                    rs.getDouble("stock_qty"),
+                    rs.getDouble("cost")));
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return foods;
     }
-
+    /**
+     * Remove a food_stock record
+     * @param id food_ID
+     */
     public void deleteFood(int id) {
         String sql = "DELETE FROM food_stock WHERE food_id = ?";
         try (Connection conn = DBConnector.getConnection();
@@ -56,58 +71,146 @@ public class FoodStockModel {
             e.printStackTrace();
         }
     }
-    public void updateFood(int id, double newCost, double newQty) {
-        String sql = "UPDATE food_stock SET cost = ?, stock_qty = ? WHERE food_id = ?";
-        try (Connection conn = DBConnector.getConnection();
-            PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
-            pstmt.setDouble(1, newCost);
-            pstmt.setDouble(2, newQty);
-            pstmt.setInt(3, id);
-            pstmt.executeUpdate();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
+    /**
+     * Updates a food_stock record
+     * @param id
+     * @param fieldName
+     * @param newValue
+     * @return
+     */
     public String updateFoodField(int id, String fieldName, String newValue) {
-        List<String> allowed = List.of("food_name", "date_expiry", "date_bought", "cost", "caloric_count", "stock_qty");
-        String message;
+        List<String> allowed = List.of(
+            "food_name", "date_expiry", "date_bought", "cost", "caloric_count", "stock_qty"
+        );
         if (!allowed.contains(fieldName)) {
-            return message = String.format("Invalid field: " + fieldName);
+            return "Invalid field: " + fieldName;
         }
 
         String sql = "UPDATE food_stock SET " + fieldName + " = ? WHERE food_id = ?";
 
         try (Connection conn = DBConnector.getConnection();
-            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
-            // handle numeric fields
             if (fieldName.equals("cost") || fieldName.equals("caloric_count") || fieldName.equals("stock_qty")) {
                 pstmt.setDouble(1, Double.parseDouble(newValue));
-            }
-            // handle date fields
-            else if (fieldName.equals("date_expiry") || fieldName.equals("date_bought")) {
+            } else if (fieldName.equals("date_expiry") || fieldName.equals("date_bought")) {
                 pstmt.setDate(1, java.sql.Date.valueOf(newValue));
-            }
-            // handle string field
-            else {
+            } else {
                 pstmt.setString(1, newValue);
             }
 
             pstmt.setInt(2, id);
             int rows = pstmt.executeUpdate();
 
-            if (rows > 0) return message = String.format("Updated " + fieldName + " for food ID " + id);
-            else return message = String.format("No food found with ID " + id);
+            if (rows > 0)
+                return "Updated " + fieldName + " for food ID " + id;
+            else
+                return "No food found with ID " + id;
 
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (Exception e) {
             System.err.println("Invalid input format for field: " + fieldName);
         }
-        return "Error!";
+        return "Error updating field.";
+    }
+    /**
+     * Get the name of a specific food_stock record.
+     */
+    public String getFoodName(int id) {
+        String sql = "SELECT food_name FROM food_stock WHERE food_id = ?";
+        try (Connection conn = DBConnector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, id);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) return rs.getString("food_name");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "(Unknown)";
     }
 
+    /**
+     * Returns a map of all allergens (description -> id) 
+    */
+    public Map<String, Integer> getAllAllergens() throws SQLException {
+        Map<String, Integer> map = new HashMap<>();
+        String sql = "SELECT allergen_id, description FROM allergen ORDER BY description";
+        try (Connection conn = DBConnector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                map.put(rs.getString("description"), rs.getInt("allergen_id"));
+            }
+        }
+        return map;
+    }
+
+    /** 
+     * Returns a list of allergen descriptions linked to a food item 
+    */
+    public List<String> getLinkedAllergens(int foodId) throws SQLException {
+        List<String> list = new ArrayList<>();
+        String sql = "SELECT a.description FROM allergen a "
+                   + "JOIN food_allergen fa ON a.allergen_id = fa.allergen_id "
+                   + "WHERE fa.food_id = ?";
+        try (Connection conn = DBConnector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, foodId);
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    list.add(rs.getString("description"));
+                }
+            }
+        }
+        return list;
+    }
+
+    /** 
+     * Link an allergen to a food item 
+    */
+    public void addAllergenToFood(int foodId, int allergenId) throws SQLException {
+        String sql = "INSERT INTO food_allergen (food_id, allergen_id) VALUES (?, ?)";
+        try (Connection conn = DBConnector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, foodId);
+            ps.setInt(2, allergenId);
+            ps.executeUpdate();
+        }
+    }
+
+    /** 
+     * Remove an allergen link from a food item 
+    */
+    public void removeAllergenFromFood(int foodId, int allergenId) throws SQLException {
+        String sql = "DELETE FROM food_allergen WHERE food_id = ? AND allergen_id = ?";
+        try (Connection conn = DBConnector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, foodId);
+            ps.setInt(2, allergenId);
+            ps.executeUpdate();
+        }
+    }
+    /**
+     * Get the sum of costs
+     * @return
+     */
+    public double getTotalCost() {
+        String sql = "SELECT SUM(cost) AS total FROM food_stock";
+        try (Connection conn = DBConnector.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery()) {
+
+            if (rs.next()) {
+                return rs.getDouble("total");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0.0;
+    }
 }
--- view/FoodStockView.java
+++ view/FoodStockView.java
@@ -5,12 +5,16 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.List;
 
+/**
+ * Main view for food_stock table
+ */
 public class FoodStockView extends JFrame {
     private DefaultListModel<String> foodListModel;
     private JList<String> foodList;
 
-    private JButton addButton, removeButton, refreshButton, editButton;
-    private JTextField nameField, expiryField, costField, calField, boughtField, qtyField;
+    private JButton addButton, removeButton, refreshButton, editButton, allergenButton;
+    private JTextField nameField, expiryField, boughtField, costField, calField, qtyField;
+    private JLabel totalCostLabel;
 
     public FoodStockView() {
         super("Food Stock Manager");
@@ -27,15 +31,40 @@ public class FoodStockView extends JFrame {
     public String getQtyInput() { return qtyField.getText(); }
     public int getSelectedIndex() { return foodList.getSelectedIndex(); }
     public String getSelectedFood() { return foodList.getSelectedValue(); }
+    public int getSelectedFoodId() {
+        String selected = foodList.getSelectedValue();
+        if (selected == null) return -1;
+        try {
+            return Integer.parseInt(selected.split("\\|")[0].replace("ID:", "").trim());
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
 
     private void initComponents() {
         foodListModel = new DefaultListModel<>();
         foodList = new JList<>(foodListModel);
 
-        addButton = new JButton("Add");
+        addButton = new JButton("Add Product");
+        addButton.setBackground(new Color(32,117,111));
+        addButton.setForeground(new Color(255, 255, 245));
+
         removeButton = new JButton("Remove");
+        removeButton.setBackground(new Color(200,40,33));
+        removeButton.setForeground(new Color(255, 255, 245));
+
         refreshButton = new JButton("Refresh");
+        refreshButton.setBackground(new Color (207, 172, 72));
+        refreshButton.setForeground(new Color(255, 255, 245));
+
         editButton = new JButton("Edit"); 
+        editButton.setBackground(new Color(32,117,111));
+        editButton.setForeground(new Color(255, 255, 245));
+
+        allergenButton = new JButton("View Allergens");
+        allergenButton.setBackground(new Color(200,40,33));
+        allergenButton.setForeground(new Color(255, 255, 245));
 
         nameField = new JTextField(10);
         expiryField = new JTextField(10);
@@ -43,39 +72,91 @@ public class FoodStockView extends JFrame {
         calField = new JTextField(5);
         boughtField = new JTextField(10);
         qtyField = new JTextField(5);
+        totalCostLabel = new JLabel("Total Cost: 0.00");
 
-        setSize(800, 500);
+        setSize(1500, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
     }
 
     private void layoutComponents() {
         setLayout(new BorderLayout());
+        setBackground(new Color(163,198,181));
 
-        JPanel formPanel = new JPanel(new GridLayout(2, 6, 5, 5));
-        formPanel.add(new JLabel("Name"));
-        formPanel.add(new JLabel("Expiry (YYYY-MM-DD)"));
-        formPanel.add(new JLabel("Bought Date"));
-        formPanel.add(new JLabel("Cost"));
-        formPanel.add(new JLabel("Calories"));
-        formPanel.add(new JLabel("Quantity"));
+        JPanel inputPanel = new JPanel(new GridLayout(2, 6, 5, 5));
+        inputPanel.setBorder(BorderFactory.createTitledBorder("Food Details"));
+        inputPanel.setBackground(new Color(255,255,245));
 
-        formPanel.add(nameField);
-        formPanel.add(expiryField);
-        formPanel.add(boughtField);
-        formPanel.add(costField);
-        formPanel.add(calField);
-        formPanel.add(qtyField);
+        Color green = new Color(207, 233, 213); 
+        JLabel nameLbl = new JLabel("Name");
+        nameLbl.setOpaque(true);
+        nameLbl.setBackground(green);
+
+        JLabel expiryLbl = new JLabel("Expiry (YYYY-MM-DD)");
+        expiryLbl.setOpaque(true);
+        expiryLbl.setBackground(green);
+
+        JLabel boughtLbl = new JLabel("Bought Date (YYYY-MM-DD)");
+        boughtLbl.setOpaque(true);
+        boughtLbl.setBackground(green);
+
+        JLabel costLbl = new JLabel("Cost (PHP)");
+        costLbl.setOpaque(true);
+        costLbl.setBackground(green);
+
+        JLabel calLbl = new JLabel("Calories (kCal)");
+        calLbl.setOpaque(true);
+        calLbl.setBackground(green);
+
+        JLabel qtyLbl = new JLabel("Quantity (grams)");
+        qtyLbl.setOpaque(true);
+        qtyLbl.setBackground(green);
+        
+        inputPanel.add(nameLbl);
+        inputPanel.add(nameField);
+        inputPanel.add(expiryLbl);
+        inputPanel.add(expiryField);
+        inputPanel.add(boughtLbl);
+        inputPanel.add(boughtField);
+        inputPanel.add(costLbl);
+        inputPanel.add(costField);
+        inputPanel.add(calLbl);
+        inputPanel.add(calField);
+        inputPanel.add(qtyLbl);
+        inputPanel.add(qtyField);
 
         JPanel buttonPanel = new JPanel();
+        buttonPanel.setBackground(new Color(255,255,245));
+
         buttonPanel.add(addButton);
         buttonPanel.add(removeButton);
         buttonPanel.add(refreshButton);
         buttonPanel.add(editButton);   
+        buttonPanel.add(allergenButton);
 
-        add(formPanel, BorderLayout.NORTH);
-        add(new JScrollPane(foodList), BorderLayout.CENTER);
-        add(buttonPanel, BorderLayout.SOUTH);
+        JPanel listPanel = new JPanel(new BorderLayout());
+        listPanel.setBorder(BorderFactory.createTitledBorder("Current Food Stock"));
+        listPanel.setBackground(new Color(255,255,245));
+        foodList.setBackground(new Color(255,255,245));  
+        foodList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        JScrollPane scroll = new JScrollPane(foodList);
+        scroll.getViewport().setBackground(new Color(255,255,245));
+
+        listPanel.add(scroll, BorderLayout.CENTER);
+
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        bottomPanel.setBackground(new Color(163,198,181));
+
+        bottomPanel.add(buttonPanel, BorderLayout.WEST);
+        JPanel costPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        costPanel.setBackground(new Color(255,255,245));
+
+        costPanel.add(totalCostLabel);
+        bottomPanel.add(costPanel, BorderLayout.EAST);
+
+        add(inputPanel, BorderLayout.NORTH);
+        add(listPanel, BorderLayout.CENTER);
+        add(bottomPanel, BorderLayout.SOUTH);
     }
 
     public void showMessage(String msg) {
@@ -88,22 +169,28 @@ public class FoodStockView extends JFrame {
             foodListModel.addElement(food);
     }
 
+    public void setTotalCost(double cost) {
+        totalCostLabel.setText(String.format("Total Cost: %.2f", cost));
+    }
+
     public void addActionListener(ActionListener l) {
         addButton.addActionListener(l);
         removeButton.addActionListener(l);
         refreshButton.addActionListener(l);
         editButton.addActionListener(l);
+        allergenButton.addActionListener(l);
 
         addButton.setActionCommand("Add");
         removeButton.setActionCommand("Remove");
         refreshButton.setActionCommand("Refresh");
         editButton.setActionCommand("Edit");
+        allergenButton.setActionCommand("Allergen");
     }
     
     public String[] promptEditField() {
         String[] fields = {"food_name", "date_expiry", "date_bought", "cost", "caloric_count", "stock_qty"};
 
-        JPanel panel = new JPanel(new GridLayout(2, 1, 5, 5));
+        JPanel panel = new JPanel(new GridLayout(4, 1, 5, 5));
         JComboBox<String> fieldDropdown = new JComboBox<>(fields);
         JTextField newValueField = new JTextField(10);
 
@@ -112,8 +199,7 @@ public class FoodStockView extends JFrame {
         panel.add(new JLabel("Enter new value:"));
         panel.add(newValueField);
 
-        int result = JOptionPane.showConfirmDialog(
-            this, panel, "Edit Food Field", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+        int result = JOptionPane.showConfirmDialog(this, panel, "Edit Food Field", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
 
         if (result == JOptionPane.OK_OPTION) {
             String field = (String) fieldDropdown.getSelectedItem();
